dd0da61a9210756126d9512e1c0098f88e125af3
FelismeniaBerwyn <201910432@gordoncollege.edu.ph>
2022-03-16 12:54:28 +0800

    create post with recipe

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,37 +3,83 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Recipe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
     public function store(Request $request)
     {
-        $post = new Post;
+        // if ($request->recipe) {
+        //     dd('true');
+        // }
+        // dd(json_decode($request->recipe, true));
+        DB::beginTransaction();
+        try {
 
-        if ($request->hasFile('post_image')) {
-            $completeFileName = $request->file('post_image')->getClientOriginalName();
-            $filenameOnly = pathinfo($completeFileName, PATHINFO_FILENAME);
-            $fileExtensionOnly = $request->file('post_image')->getClientOriginalExtension();
+            // $post = new Post;
 
-            // $compPic = str_replace(' ', '_', $filenameOnly) . '-' . rand() . '_' . time() . '.' . $fileExtensionOnly;
-            $compPic = str_replace(' ', '_', $filenameOnly) . '_' . time() . '.' . $fileExtensionOnly;
-            // $compPic = now()->timestamp . '_' . str_replace(' ', '_', $filenameOnly);
+            if ($request->hasFile('post_image')) {
+                $completeFileName = $request->file('post_image')->getClientOriginalName();
+                $filenameOnly = pathinfo($completeFileName, PATHINFO_FILENAME);
+                $fileExtensionOnly = $request->file('post_image')->getClientOriginalExtension();
 
-            // dd($completeFileName, $filenameOnly, $fileExtensionOnly, $compPic);
+                // $compPic = str_replace(' ', '_', $filenameOnly) . '-' . rand() . '_' . time() . '.' . $fileExtensionOnly;
+                $compPic = str_replace(' ', '_', $filenameOnly) . '_' . time() . '.' . $fileExtensionOnly;
+                // $compPic = now()->timestamp . '_' . str_replace(' ', '_', $filenameOnly);
 
-            $path = $request->file('post_image')->storeAs('public/posts', $compPic);
+                // dd($completeFileName, $filenameOnly, $fileExtensionOnly, $compPic);
 
-            $post->user_id = $request->user()->id;
-            $post->caption = $request->caption;
-            $post->post_image = $compPic;
-        }
+                $path = $request->file('post_image')->storeAs('public/posts', $compPic);
 
-        if ($post->save()) {
+                // using eloquent
+                // $post->user_id = $request->user()->id;
+                // $post->caption = $request->caption;
+                // $post->post_image = $compPic;
+
+                $post_id = DB::table('posts')->insertGetId(array(
+                    'user_id' => $request->user()->id,
+                    'caption' => $request->caption,
+                    'post_image' => $compPic,
+                    "created_at" =>  \Carbon\Carbon::now(),
+                    "updated_at" => \Carbon\Carbon::now(),
+                ));
+
+                if ($request->recipe) {
+                    $data = json_decode($request->recipe, true);
+                    DB::table('recipes')->insertGetId(array(
+                        'post_id' => $post_id,
+                        'ingredient' => json_encode($data['ingredients']),
+                        'procedure' => json_encode($data['procedures']),
+                        "created_at" =>  \Carbon\Carbon::now(),
+                        "updated_at" => \Carbon\Carbon::now(),
+                    ));
+                }
+            }
+            DB::commit();
             return ['status' => 'success', 'message' => 'Post saved successfully'];
-        } else {
+
+
+            // using eloquent
+            // if ($post->save()) {
+            //     if ($request->recipe) {
+            //         $recipe = new Recipe;
+            //         $data = json_decode($request->recipe, true);
+            //         // dd(json_encode($data['procedures']));
+            //         // $post->id;
+            //         $recipe->post_id = $post->id;
+            //         $recipe->ingredient = json_encode($data['ingredients']);
+            //         $recipe->procedure = json_encode($data['procedures']);
+            //         return ['status' => 'success', 'message' => 'Post saved successfully'];
+            //     }
+            // } else {
+            //     return ['status' => 'Failed', 'message' => 'Something went wrong'];
+            // }
+        } catch (\Exception $e) {
             return ['status' => 'Failed', 'message' => 'Something went wrong'];
+            DB::rollback();
         }
     }
 
---
+++ app/Models/Recipe.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'procedure',
+        'ingredient',
+    ];
+}
