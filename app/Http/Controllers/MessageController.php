983fd7d2a3a78b19ed76a036ac1d31d0ed386c35
FelismeniaBerwyn <201910432@gordoncollege.edu.ph>
2022-05-05 15:37:32 +0800

    update message controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
@@ -15,14 +17,34 @@ class MessageController extends Controller
             $criteria_id = $request->from . $request->to;
         }
 
-        $messages = Message::where('criteria_id', $criteria_id)->skip($request->get('skip'))->take(9)->orderBy('created_at', 'desc')->get();
+        $messages = Message::where('criteria_id', $criteria_id)->skip($request->get('skip'))->take(9)->get();
 
-        return ['status' => 'success', 'data' => $messages];
+        $structMessage = array();
+        foreach ($messages as $key => $message) {
+            array_push($structMessage, [
+                'message' => $messages[$key]->message,
+                'time' => Carbon::createFromTimeStamp(strtotime($messages[$key]->created_at))->diffForHumans(),
+                'userid' => $messages[$key]->from,
+            ]);
+        }
+
+        return ['status' => 'success', 'data' => $structMessage];
     }
 
     public function checkInbox(Request $request)
     {
         $messageFrom = Message::where('from', $request->from)->orwhere('to', $request->from)->orderBy('created_at', 'desc')->get()->unique('criteria_id');
+
+        foreach ($messageFrom as $key => $message) {
+            if ($request->from !== $message->from) {
+                // array_push($arr, $message);
+                $user = User::find($message->from);
+                $messageFrom[$key]['user'] = $user;
+            } else if ($request->from !== $message->to) {
+                $user = User::find($message->to);
+                $messageFrom[$key]['user'] = $user;
+            }
+        }
 
         return ['status' => 'success', 'data' => $messageFrom];
     }
